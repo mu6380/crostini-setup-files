1861731b71eae36496f0bed441db1c179ab1cb20
mu6380 <85578048+mu6380@users.noreply.github.com>
2021-06-24 19:27:03 -0400

    Add files via upload

---
+++ mknotes.py
@@ -0,0 +1,64 @@
+import os
+import time
+from os.path import expanduser
+from datetime import date
+
+subject_dictionary = {
+  "biology": ["/latex.files/notes/biology.notes", "normal"],
+  "math": ["/latex.files/notes/math.notes", "normal"],
+  "sstudies": ["/latex.files/notes/sstudies.notes", "normal"],
+  "physics": ["/latex.files/notes/physics.notes", "normal"],
+  "sandboxes": ["/latex.files/sandboxes", "other"],
+  "projects": ["/latex.files/projects", "other"],
+  "chemistry": ["/latex.files/notes/chemistry.notes", "normal"],
+}
+
+home = expanduser("~")
+
+# make all the needed subject_directories
+for x in subject_dictionary.keys():
+    fulldir=home+subject_dictionary[x][0]
+    if os.path.isdir(fulldir) == False:
+        print("Made directory: "+fulldir)
+        os.makedirs(fulldir)
+
+date=date.today().strftime("%Y-%d-%m")
+subject=input("Where will the note be?\n")
+extension=""
+
+# check if subject names are valid
+if subject in subject_dictionary:
+    # define the procedure type for which the note will be made with
+    print("Nice job, it's a real location.")
+else:
+    print("wtf that's not a location")
+    exit()
+
+if subject_dictionary[subject][1] == "normal":
+    
+    foldername=input("Note folder name?\n")
+    notename=input("Note name?\n")
+
+    foldername=foldername.replace("--date", date)
+    notename=notename.replace("--date", date)
+
+    if "." not in notename:
+        extension=".tex"
+    notefolder=home+subject_dictionary[subject][0]+'/'+foldername
+    os.makedirs(notefolder)
+    open(notefolder+"/"+notename+extension, 'x')
+
+    print("Made directory: "+notefolder)
+    print("Made note: "+notename+extension)
+
+if subject_dictionary[subject][1] == "other":
+    note_and_folder_name=input("Note and folder name?\n")
+
+    if "." not in note_and_folder_name:
+        extension=".tex"
+    notefolder=home+subject_dictionary[subject][0]+'/'+note_and_folder_name
+    os.makedirs(notefolder)
+    open(notefolder+"/"+note_and_folder_name+extension, 'x')
+
+    print("Made directory: "+notefolder)
+    print("Made note: "+note_and_folder_name+extension)
---
+++ mknotes.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+if [ ! -d "~/latex.files/notes/biology.notes" ]
+then
+mkdir -p ~/latex.files/notes/biology.notes
+fi
+
+if [ ! -d "~/latex.files/notes/math.notes" ]
+then
+mkdir -p ~/latex.files/notes/math.notes
+fi
+
+if [ ! -d "~/latex.files/notes/sstudies.notes" ]
+then
+mkdir -p ~/latex.files/notes/sstudies.notes
+fi
+
+if [ ! -d "~/latex.files/sandboxes" ]
+then
+mkdir -p ~/latex.files/sandboxes
+fi
+
+if [ ! -d "~/latex.files/projects" ]
+then
+mkdir -p ~/latex.files/projects
+fi
+
+############################################################
+
+echo Where?
+date=$(date | cut -d " " -f 3,2,4)
+read destination
+
+############################################################
+
+if [ "$destination" == "biology" ];
+then
+echo "note folder name?"
+read notefolder
+cd ~/latex.files/notes/biology.notes
+preceding=$(echo $notefolder | awk -F "--date" '{print $1}')
+succeeding=$(echo $notefolder | awk -F "--date" '{print $2}')
+if [[ "$notefolder" == *"--date"* ]]
+        then
+        mkdir "$preceding$date$succeeding"
+        cd "$preceding$date$succeeding"
+        else
+        mkdir "$notefolder"
+        cd "$notefolder"
+fi
+echo "note name?"
+read notename
+stringname=$(echo $notename | rev | cut -d "." -f2- | rev)
+preceding=$(echo $stringname | awk -F "--date" '{print $1}')
+succeeding=$(echo $stringname | awk -F "--date" '{print $2}')
+extension=$(echo $notename | rev | cut -d "." -f 1 | rev)
+if [[ "$stringname" == *"--date"* ]]
+        then
+        touch "$preceding$date$succeeding.$extension"
+        atom "$preceding$date$succeeding.$extension"
+        else
+        touch "$stringname.$extension"
+        atom "$stringname.$extension"
+fi
+fi
+
+############################################################
+
+if [ "$destination" == "math" ];
+then
+echo "note folder name?"
+read notefolder
+cd ~/latex.files/notes/math.notes
+preceding=$(echo $notefolder | awk -F "--date" '{print $1}')
+succeeding=$(echo $notefolder | awk -F "--date" '{print $2}')
+if [[ "$notefolder" == *"--date"* ]]
+        then
+        mkdir "$preceding$date$succeeding"
+        cd "$preceding$date$succeeding"
+        else
+        mkdir "$notefolder"
+        cd "$notefolder"
+fi
+echo "note name?"
+read notename
+stringname=$(echo $notename | rev | cut -d "." -f2- | rev)
+preceding=$(echo $stringname | awk -F "--date" '{print $1}')
+succeeding=$(echo $stringname | awk -F "--date" '{print $2}')
+extension=$(echo $notename | rev | cut -d "." -f 1 | rev)
+if [[ "$stringname" == *"--date"* ]]
+        then
+        touch "$preceding$date$succeeding.$extension"
+        atom "$preceding$date$succeeding.$extension"
+        else
+        touch "$stringname.$extension"
+        atom "$stringname.$extension"
+fi
+fi
+
+############################################################
+
+if [ "$destination" == "sstudies" ];
+then
+echo "note folder name?"
+read notefolder
+cd ~/latex.files/notes/sstudies.notes
+preceding=$(echo $notefolder | awk -F "--date" '{print $1}')
+succeeding=$(echo $notefolder | awk -F "--date" '{print $2}')
+if [[ "$notefolder" == *"--date"* ]]
+        then
+        mkdir "$preceding$date$succeeding"
+        cd "$preceding$date$succeeding"
+        else
+        mkdir "$notefolder"
+        cd "$notefolder"
+fi
+echo "note name?"
+read notename
+stringname=$(echo $notename | rev | cut -d "." -f2- | rev)
+preceding=$(echo $stringname | awk -F "--date" '{print $1}')
+succeeding=$(echo $stringname | awk -F "--date" '{print $2}')
+extension=$(echo $notename | rev | cut -d "." -f 1 | rev)
+if [[ "$stringname" == *"--date"* ]]
+        then
+        touch "$preceding$date$succeeding.$extension"
+        atom "$preceding$date$succeeding.$extension"
+        else
+        touch "$stringname.$extension"
+        atom "$stringname.$extension"
+fi
+fi
+
+############################################################
+
+if [ "$destination" == "sandboxes" ];
+then
+    echo "note folder+note name?"
+    read notefolder
+    cd ~/latex.files/sandboxes
+    mkdir "$notefolder"
+    cd "$notefolder"
+    touch "$notefolder.tex"
+    atom "$notefolder.tex"
+fi
+
+############################################################
+
+if [ "$destination" == "projects" ];
+then
+echo "note folder name?"
+read notefolder
+cd ~/latex.files/projects
+preceding=$(echo $notefolder | awk -F "--date" '{print $1}')
+succeeding=$(echo $notefolder | awk -F "--date" '{print $2}')
+if [[ "$notefolder" == *"--date"* ]]
+        then
+        mkdir "$preceding$date$succeeding"
+        cd "$preceding$date$succeeding"
+        else
+        mkdir "$notefolder"
+        cd "$notefolder"
+fi
+echo "note name?"
+read notename
+stringname=$(echo $notename | rev | cut -d "." -f2- | rev)
+preceding=$(echo $stringname | awk -F "--date" '{print $1}')
+succeeding=$(echo $stringname | awk -F "--date" '{print $2}')
+extension=$(echo $notename | rev | cut -d "." -f 1 | rev)
+if [[ "$stringname" == *"--date"* ]]
+        then
+        touch "$preceding$date$succeeding.$extension"
+        atom "$preceding$date$succeeding.$extension"
+        else
+        touch "$stringname.$extension"
+        atom "$stringname.$extension"
+fi
+fi
+
+############################################################
+
+if [ "$destination" != "biology" ] && [ "$destination" != "math" ] && [ "$destination" != "sandboxes" ] && [ "$destination" != "sstudies" ] && [ "$destination" != "projects" ];
+then
+echo "wtf that's not a directory"
+fi
